93afdfe78bf79c8313a867694e8a9bad10de5fd5
Avramenko_TR-11 <avramenko.oleg@lll.kpi.ua>
2025-12-04 03:58:47 +0200

    Finished Practice 5

--- Practice1/obj/Debug/net9.0/Practice1.AssemblyInfo.cs
+++ Practice1/obj/Debug/net9.0/Practice1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8acda0450f51b386db1e1f45ed343de3fbbae660")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e3a6a25a99a254cfdfafbe50b2a0ff359e1657c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
